0513536328b96aaab5f4f71e072019d4e86d01da
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-04-02 03:04:10 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-数据模型模块
-"""
-from models.user import User, UserRole
-from models.report import Report, ReportStatus
-from models.template import Template
-
-__all__ = ["User", "UserRole", "Report", "ReportStatus", "Template"]
